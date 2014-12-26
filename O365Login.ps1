1403a26d8787e092becd4c543cb454e46d20b901
Joe Frixon <frixon@gmail.com>
2014-12-26 18:25:13 +0530

    Office 365 login & logout

---
+++ O365Login.ps1
@@ -0,0 +1,3 @@
+﻿$LiveCred = Get-Credential
+$Session7 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+Import-PSSession $Session7
--- default.ps1
+++
@@ -1,3 +0,0 @@
-﻿$LiveCred = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
-Import-PSSession $Session7
