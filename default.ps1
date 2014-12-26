7a287c00a262fd4464e058422d5db6ddf43e8c3c
Joe Frixon <frixon@gmail.com>
2014-12-26 11:31:09 +0530

    Connection String

---
+++ default.ps1
@@ -0,0 +1,3 @@
+﻿$LiveCred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+Import-PSSession $Session7
